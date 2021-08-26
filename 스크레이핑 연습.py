6865fbdffc482c05378fe11431646cf9fa5232bf
IDF13 <tdg01192@gmail.com>
2021-08-26 14:07:14 +0900

    스크레이핑 코드 추가

--- 스크레이핑 연습.py
+++ 스크레이핑 연습.py
@@ -7,4 +7,8 @@ soup_web_ranking = BeautifulSoup(html_web_ranking, 'lxml')
 
 # p 태그 요소 안에서 a 태그의 요소를 찾기
 web_ranking = soup_web_ranking.select('p a')
-print(web_ranking)
+print(web_ranking[1].get_text())
+
+website_ranking = [web_ranking_element.get_text() for web_ranking_element in web_ranking[1:]]
+
+print(website_ranking[:4])
