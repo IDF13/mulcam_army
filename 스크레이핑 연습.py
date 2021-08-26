5ec4c0dfe1d143a1dcc60dcdb65debb23f1d8a00
IDF13 <tdg01192@gmail.com>
2021-08-26 14:00:50 +0900

    스크레이핑 연습

---
+++ 스크레이핑 연습.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.alexa.com/topsites/countries/KR'
+html_web_ranking = requests.get(url).text
+soup_web_ranking = BeautifulSoup(html_web_ranking, 'lxml')
+
+# p 태그 요소 안에서 a 태그의 요소를 찾기
+web_ranking = soup_web_ranking.select('p a')
+print(web_ranking)
