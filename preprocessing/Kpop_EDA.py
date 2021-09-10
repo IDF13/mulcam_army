1df78c4fa564523b6c9199f369a6ad8535552de5
ssunggun2 <85274662+ssunggun2@users.noreply.github.com>
2021-09-10 16:17:34 +0900

    수정 및 도움 요청

--- preprocessing/Kpop_EDA.py
+++ preprocessing/Kpop_EDA.py
@@ -23,8 +23,10 @@ import re
 get_ipython().system('pip install nltk')
 import nltk
 nltk.download('punkt')
-
+nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 from nltk.tokenize import word_tokenize  
+from nltk.tokenize import sent_tokenize  
 
 
 # In[4]:
@@ -169,114 +171,16 @@ import kss
 # In[24]:
 
 
-punct = "/-'?!.,#$%\'()*+-/:;<=>@[\\]^_`{|}~" + '""“”’' + '∞θ÷α•à−β∅³π‘₹´°£€\×™√²—–&'
-punct_mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": " sqrt ", "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-", "`": "'", '“': '"', '”': '"', '“': '"', "£": "e", '∞': 'infinity', 'θ': 'theta', '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 'β': 'beta', '∅': '', '³': '3', 'π': 'pi', }
+#띄어쓰기 <= 필요할듯
 
 
 # In[25]:
 
 
-#이모티콘 제거
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-#                            u"\U00002702-\U000027B0"
-#                            u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
-
-
-emoji_pattern = re.compile(
-    u"(\ud83d[\ude00-\ude4f])|"  # emoticons
-    u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
-    u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
-    u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
-    u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
-    "+", flags=re.UNICODE)
+# !pip install git+https://github.com/haven-jeon/PyKoSpacing.git
 
 
 # In[26]:
-
-
-#텍스트 정제
-def clean_punc(text, punct, mapping):
-    for p in mapping:
-        text = text.replace(p, mapping[p])
-    
-    for p in punct:
-        text = text.replace(p, f' {p} ')
-    
-    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '', 'है': ''}
-    for s in specials:
-        text = text.replace(s, specials[s])
-    
-    return text.strip()
-
-
-# In[27]:
-
-
-#텍스트 정제
-def clean_text(texts):
-    corpus = []
-    for i in range(0, len(texts)):
-        review = re.sub(r'[@%\\*=()/~#&\+á?\xc3\xa1\-\|\.\:\;\!\-\,\_\~\$\'\"]', '',str(texts[i])) #remove punctuation
-#         review = re.sub(r'\d+','', str(texts[i]))# remove number
-        review = review.lower() #lower case
-        review = re.sub(r'\s+', ' ', review) #remove extra space
-        review = re.sub(r'<[^>]+>','',review) #remove Html tags
-        review = re.sub(r'\s+', ' ', review) #remove spaces
-        review = re.sub(r"^\s+", '', review) #remove space from start
-        review = re.sub(r'\s+$', '', review) #remove space from the end
-        corpus.extend(review)
-    return corpus
-
-
-# In[28]:
-
-
-train['content']
-
-
-# In[29]:
-
-
-for i in range(len(content)):
-        train.loc[i,'content']=remove_emoji(content[i])
-        train.loc[i,'content']=clean_punc(content[i], punct, punct_mapping)
-        train['content'][i]=clean_text(content[i])
-        train['content'][i]=word_tokenize(content[i])        # 토크나이징
-        
-train
-
-
-# In[30]:
-
-
-# for i in range(len(content)):
-#     train['content'][i]=clean_text(content[i])
-# # train
-
-# for i in range(len(content)):
-#     train['content'][i]=word_tokenize(content[i])
-
-
-# In[31]:
-
-
-train['content']
-
-
-# In[32]:
-
-
-#띄어쓰기
-
-
-# In[33]:
 
 
 ## 맞춤법 검사기
@@ -284,7 +188,7 @@ train['content']
 # !pip install git+https://github.com/ssut/py-hanspell.git
 
 
-# In[34]:
+# In[27]:
 
 
 # from hanspell import spell_checker
@@ -313,27 +217,316 @@ train['content']
 #     print(spell.candidates(word))
 
 
+# In[28]:
+
+
+punct = "/-'?!.,#$%\'()*+-/:;<=>@[\\]^_`{|}~" + '""“”’' + '∞θ÷α•à−β∅³π‘₹´°£€\×™√²—–&'
+punct_mapping = {"‘": "'", "₹": "e", "´": "'", "°": "", "€": "e", "™": "tm", "√": " sqrt ", "×": "x", "²": "2", "—": "-", "–": "-", "’": "'", "_": "-", "`": "'", '“': '"', '”': '"', '“': '"', "£": "e", '∞': 'infinity', 'θ': 'theta', '÷': '/', 'α': 'alpha', '•': '.', 'à': 'a', '−': '-', 'β': 'beta', '∅': '', '³': '3', 'π': 'pi', }
+
+
+# In[29]:
+
+
+#이모티콘 제거
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # emoticons
+                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+#                            u"\U00002702-\U000027B0"
+#                            u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
+
+
+emoji_pattern = re.compile(
+    u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+    u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+    u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+    u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+    u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+    "+", flags=re.UNICODE)
+
+
+# In[30]:
+
+
+#텍스트 정제
+def clean_punc(text, punct, mapping):
+    for p in mapping:
+        text = text.replace(p, mapping[p])
+    
+    for p in punct:
+        text = text.replace(p, f' {p} ')
+    
+    specials = {'\u200b': ' ', '…': ' ... ', '\ufeff': '', 'करना': '', 'है': ''}
+    for s in specials:
+        text = text.replace(s, specials[s])
+    
+    return text.strip()
+
+
+# In[31]:
+
+
+#텍스트 정제
+def clean_text(texts):
+    corpus = []
+    for i in range(0, len(texts)):
+        review = re.sub(r'[@%\\*=()/~#&\+á?\xc3\xa1\-\|\.\:\;\!\-\,\_\~\$\'\"]', '',str(texts[i])) #remove punctuation
+#         review = re.sub(r'\d+','', str(texts[i]))# remove number
+        review = review.lower() #lower case
+        review = re.sub(r'\s+', ' ', review) #remove extra space
+        review = re.sub(r'<[^>]+>','',review) #remove Html tags
+        review = re.sub(r'\s+', ' ', review) #remove spaces
+        review = re.sub(r"^\s+", '', review) #remove space from start
+        review = re.sub(r'\s+$', '', review) #remove space from the end
+        corpus.extend(review)
+    return corpus
+
+
+# In[32]:
+
+
+train['content']
+
+
 # In[35]:
+
+
+train['content_clean']=''
+for i in range(len(content)):
+        train.loc[i,'content_clean']=remove_emoji(content[i])
+        train.loc[i,'content_clean']=clean_punc(train['content_clean'][i], punct, punct_mapping)
+        train['content_clean'][i]=clean_text(train['content_clean'][i])
+
+
+# In[37]:
+
+
+train
+
+
+# In[38]:
+
+
+clean=train['content_clean']
+train['content_word']=''
+train['content_sent']=''
+for i in range(len(content)):
+        train['content_word'][i]=word_tokenize(str(clean[i]))        # 토크나이징
+        train['content_sent'][i]=sent_tokenize(str(clean[i]))        # 토크나이징
+        
+train
+
+
+# In[ ]:
+
+
+train['content_word']
+
+
+# In[ ]:
+
+
+train['content_sent']
+
+
+# In[ ]:
 
 
 # for i in range(len(content)):
 #     train.loc[i,'content']=spell.correction(content[i])
 
 
-# In[36]:
+# In[ ]:
 
 
-#불용어
+#영어불용어
 
 
-# In[37]:
+# In[ ]:
+
+
+from nltk.corpus import stopwords 
+stop_words = set(stopwords.words('english')) 
+
+
+
+# result = []
+# for w in word_tokens: 
+#     if w not in stop_words: 
+#         result.append(w) 
+
+# print(word_tokens) 
+# print(result) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#한글 불용어
+
+
+# In[ ]:
+
+
+stopwords = ['데/NNB', '좀/MAG', '수/NNB', '등/NNB']
+
+
+# In[ ]:
+
+
+def remove_stopword_text(text):
+    corpus = []
+    for sent in text:
+        modi_sent = []
+        for word in sent.split(' '):
+            if word not in stopwords:
+                modi_sent.append(word)
+        corpus.append(' '.join(modi_sent))
+    return corpus
+
+
+# In[ ]:
+
+
+# removed_stopword_corpus = remove_stopword_text(stemming_corpus)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#영어 형태소 분석
+
+
+# In[ ]:
+
+
+from nltk.tag import pos_tag
+train['POS']=''
+for i in range(len(content)):
+        train['POS'][i]=pos_tag(train.loc[i,'content_word'])
+train
+
+
+# In[ ]:
+
+
+# 한글 형태소 분석
+
+
+# In[ ]:
+
+
+# # Python 기반의 형태소 분석기 중, 성능이 가장 좋은 것 중 하나인 카카오의 [Khaiii](https://github.com/kakao/khaiii)를 사용하겠습니다.
+
+# !git clone https://github.com/kakao/khaiii.git
+# !pip install cmake
+# !mkdir build
+# !cd build && cmake /content/khaiii
+# !cd /content/build/ && make all
+# !cd /content/build/ && make resource
+# !cd /content/build && make install
+# !cd /content/build && make package_python
+# !pip install /content/build/package_python
+
+
+# In[ ]:
+
+
+get_ipython().system('pip install konlpy')
+
+
+# In[ ]:
+
+
+#그냥 konlpy씀
+from konlpy.tag import Okt  
+okt=Okt()  
+
+
+# In[ ]:
+
+
+for k in train['len']:
+    if train['len'][k]!='0':
+        for i in range(len(content)):
+            train['POS'][i]=okt.pos(content[i])
+    else:
+        pass
+
+train
+
+
+# In[ ]:
+
+
+# def pos_text(texts):
+#     corpus = []
+#     for sent in texts:
+#         pos_tagged = ''
+#         for word in api.analyze(sent):
+#             for morph in word.morphs:
+#                 if morph.tag in significant_tags:
+#                     pos_tagged += morph.lex + '/' + morph.tag + ' '
+#         corpus.append(pos_tagged.strip())
+#     return corpus
+
+
+# In[ ]:
+
+
+# from khaiii import KhaiiiApi
+# api = KhaiiiApi()
+# if train['len']!=0:
+#     for i in range(len(content)):
+#         train.loc[i,'POS']=pos_text(content[i])
+# else:
+#     pass
+
+# train
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 #정수 인코딩
-
-
-# In[38]:
-
-
-#형태소 분리
 
