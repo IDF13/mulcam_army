61be77a705cbb0c1ffaeef5f3f247d9aac28d8fe
solleedata <solleedata@gmail.com>
2021-09-16 15:03:11 +0900

    '천', '만' -> 000, 0000

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from textblob import TextBlob
 
-# 아래 두 줄만 바꿔주시면 됩니다!
+# 여기만 바꿔주시면 됩니다!
 df = pd.read_csv(
-    '/Users/sollee/Desktop/mulcam_army/youtube_dataset/comments/comments_TWICE.csv')
+    '/Users/sollee/Desktop/mulcam_army/youtube_dataset/comments/comments_SUPER JUNIOR.csv')
 texts = df.iloc[:, 0]
 likes = df.iloc[:, 1]
 
@@ -11,6 +11,10 @@ clean_tweets = []
 scores = []
 likes_list = []
 emotions = []
+
+likes = likes.fillna(0)
+value_list = likes.values.tolist()
+value_list = [str(value) for value in value_list]
 
 
 def sentiment_analyzer():
@@ -20,28 +24,31 @@ def sentiment_analyzer():
         score = round(blob.sentiment.polarity, 2)
         scores.append(score)
 
-    for like in likes:
-        likes_list.append(like)
+    # for like in likes:
+    #     likes_list.append(like)
+
+    for value in value_list:
+        result = 0
+        num_map = {'천': 1000, '만': 10000}
+        if value.isdigit():
+            result = int(value)
+        else:
+            if len(value) > 1:
+                result = float(value[:-1])*num_map.get(value[-1].upper(), 1)
+        likes_list.append(int(result))
 
     for score, like in zip(scores, likes_list):
-        emotions.append(score*like)
+        emotions.append(round(score*like, 2))
 
     table = pd.DataFrame(
         [clean_tweets, likes_list, scores, emotions]).T
-    table.columns = ['original texts', '좋아요', '긍정점수', '공감지수']
+    table.columns = ['original texts', '👍 좋아요',
+                     '⭐️ 긍정점수(textblob)', '😍 공감지수(좋아요 수 x 긍정점수)']
 
     print(table)
 
-    table.to_csv(f'/Users/sollee/Desktop/sentiment_analysis_TWICE.csv')
+    table.to_csv(f'/Users/sollee/Desktop/sentiment_analysis_SUJU.csv')
     return table
 
 
 sentiment_analyzer()
-
-# 결과 예시:
-'''
-original texts                                                     sentiment score
-0     Ando full, pero ahí ando que le hago tiempo pa...             0.5
-1     Un Super Clap para ELF vamos ELF que nunca fal...            0.33
-2     Siwon jugando tenis... que elegancia ...                     0.25
-'''
