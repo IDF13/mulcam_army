8cd4f03fcc2facf081190348a50122d3b864afa2
Sol Lee <82362278+solleedata@users.noreply.github.com>
2021-10-01 14:42:21 +0900

    Update youtube_crawling_views_likes_dislikes.py

--- youtube_crawlers/youtube_crawling_views_likes_dislikes.py
+++ youtube_crawlers/youtube_crawling_views_likes_dislikes.py
@@ -1,153 +1,108 @@
-from http.client import responses
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from httplib2 import RETRIES
-from oauth2client import service_account
-from oauth2client.tools import argparser
-from requests.api import request
 import pandas as pd
+from googleapiclient.discovery import build
 
-DEVELOPER_KEY = "AIzaSyBairE2KSXeJp6jNHw6NCBiip8YvzLGsDA"
-YOUTUBE_API_SERVICE_NAME="youtube"
-YOUTUBE_API_VERSION="v3"
+# API Key (일일 할당량 있음)
+DEVELOPER_KEY = "AIzaSyDO7dPJcu4TqdRXu3kvz4KIZpGtVPxeYoo"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+URL_FORMAT = 'https://www.youtube.com/watch?v='
 
-youtube = build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
 
-## Playlist Id값 구하기
+def youtube_search(options, q):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                    developerKey=DEVELOPER_KEY)
 
-# ### List   중 넥스트레벨 재생목록  데이터 / 아이디
-# 모든 list 안의 동영상 date , title, id(50개까지)
-
-def getsearch_response(query):
-    
     search_response = youtube.search().list(
-    q = {query},
-    order = "relevance",
-    part = "snippet",
-    maxResults = 50
+        q=q,
+        order='relevance',
+        part="id,snippet",
+        maxResults=4
     ).execute()
 
-    # print(search_response)
-    return search_response
+    videos = []
+    video_ids = []
+    channels = []
+    playlists = []
 
-def getchannelId(query):
-    channelId = getsearch_response(query)['items'][0]['snippet']['channelId']
-    return channelId
-def gettitle(query):
-    title = getsearch_response(query)['items'][0]['snippet']['title']
-    return title
-def getdescription(query):
-    description = getsearch_response(query)['items'][0]['snippet']['description']
-    return description
-def getthumbnails_high(query):
-    thumbnails_high = getsearch_response(query)['items'][0]['snippet']['thumbnails']['high']['url']
-    return thumbnails_high
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                       search_result["id"]["videoId"]))
+            video_ids.append(search_result["id"]["videoId"])
+        elif search_result["id"]["kind"] == "youtube#channel":
+            channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                         search_result["id"]["channelId"]))
+        elif search_result["id"]["kind"] == "youtube#playlist":
+            playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                          search_result["id"]["playlistId"]))
 
+    print("----- Videos:------\n", "\n".join(videos), "\n")
+    print("----- Channels:------\n", "\n".join(channels), "\n")
+    print("----- Playlists:-------\n", "\n".join(playlists), "\n")
 
+    print('*'*80)
 
+    # print(search_result['id']['kind'])
+    # print(search_result['id'])
+    # print(video_ids)
 
-def playlists(query):
-        
-    playlists = youtube.playlists().list(
-        channelId=getchannelId(query)
-        , part= "snippet"
-        , maxResults=50
-    ).execute()
+    ##
 
-    ids=[]
-    titles=[]
-    description=[]
-    thumbnails=[]
+    category_id = []
+    views = []
+    likes = []
+    dislikes = []
+    comments = []
+    title = []
+    url = []
 
-    try:
-        for i in playlists['items']:
-            ids.append(i['id'])
-            titles.append(i['snippet']['title'])
-            description.append(i['snippet']['description'])
-            thumbnails.append(i['snippet']['thumbnails']['default']['url'])
-    except:
-        pass
-
-    df=pd.DataFrame([ids, titles,description,thumbnails]).T
-    df.columns=['PlayLists', 'Titles','Description','Thumbnails']
-    # print(df)
-    return df
-    
-
-
-
-def getvideolist(query):
-    for i in range(len(playlists(query)['PlayLists'])):
-        getchannelid = playlists(query)['PlayLists'][i]
-        playlist_videos=youtube.playlistItems().list(
-            playlistId=getchannelid
-            ,part = 'snippet'
-            ,maxResults=50
-        )
-        playlistitems_list_response = playlist_videos.execute()
-
-        video_names=[]
-        video_ids=[]
-        date=[]
-
-        for v in playlistitems_list_response['items']:
-            video_names.append(v['snippet']['title'])
-            video_ids.append(v['snippet']['resourceId']['videoId'])
-            date.append(v['snippet']['publishedAt'])
-
-        vdf= pd.DataFrame([date, video_names, video_ids]).T
-        vdf.columns=['Date','Title','IDS']
-        vdf.to_csv(f'{query}.csv')
-        # print(vdf)
-        
-        return vdf
-
- 
-
-###  재생목록 중 동영상들 stats
-
-import re
-
-def getstatistics(query):
-        
-    ids=[]
-    category_id=[]
-    views=[]
-    likes=[]
-    dislikes=[]
-    comments=[]
-    mins=[]
-    seconds=[]
-    title=[]
-
-    for u in range(len(getvideolist(query))):
+    for i in range(len(video_ids)):
         request = youtube.videos().list(
-            part='snippet,contentDetails,statistics'
-            ,id = getvideolist(query)['IDS'][u]
+            part='snippet,contentDetails,statistics',
+            id=video_ids[i]
         )
 
         response = request.execute()
+        ###
 
-        if response['items']==[]:
-            ids.append("-")
-            category_id.append("-")
-            views.append("-")
-            likes.append("-")
-            dislikes.append("-")
-            comments.append("-")
-
+        if response['items'] == []:
+            category_id.append('-')
+            views.append('-')
+            likes.append('-')
+            dislikes.append('-')
+            comments.append('-')
+            url.append('-')
         else:
-
             title.append(response['items'][0]['snippet']['title'])
             category_id.append(response['items'][0]['snippet']['categoryId'])
             views.append(response['items'][0]['statistics']['viewCount'])
             likes.append(response['items'][0]['statistics']['likeCount'])
             dislikes.append(response['items'][0]['statistics']['dislikeCount'])
             comments.append(response['items'][0]['statistics']['commentCount'])
+            url.append(URL_FORMAT + video_ids[i])
 
-    statistics_df=pd.DataFrame([title, category_id, views,likes, dislikes, comments]).T
-    statistics_df.columns=['Title','Category_id','Views','Likes','Dislikes','Comments']
-    statistics_df.to_csv(f'{query}_statistics.csv')
-    print(statistics_df)
-    return statistics_df
+    table = pd.DataFrame(
+        [title, views, likes, dislikes, comments, video_ids, url]).T
+    table.columns = ['제목', '조회수', '좋아요', '싫어요', '댓글수', '영상아이디', '영상url']
 
+    print('++++++++ table ++++++++++')
+    print(table)
+    print()
+
+    table.to_csv(f'youtube_{title}.csv')
+    return table
+##
+
+
+query_list = []
+artist_list = pd.read_csv(
+    '/Users/sollee/Desktop/mulcam_army/blip_artists_list.csv')
+
+for idx in range(len(query_list)):
+    artist_kor = artist_list['artist_kor'][idx]
+    artist_eng = artist_list['artist_eng'][idx]
+    query_list.append(artist_eng)
+    try:
+        youtube_search(options=None, q=query_list[idx])
+    except Exception:
+        pass
